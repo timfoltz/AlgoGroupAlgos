6e9bb2cb57b66d0beabc947459fdcfe2185db471
timfoltz <73617242+timfoltz@users.noreply.github.com>
2021-03-26 10:36:22 -0700

    update

---
+++ Javascript/integerBreak.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/integer-break/
+// 343. Integer Break
+// Medium
+// Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+// Return the maximum product you can get.
+
+// Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: 2 = 1 + 1, 1 × 1 = 1.
+
+// Example 2:
+// Input: n = 10
+// Output: 36
+// Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+
+
+
+integerBreak = (n) => {
+    // let arr = [];
+    // for(let i = 1; i<=n; i++) {
+    //     arr.push(i);
+    // }
+    // return arr
+    
+
+     // thinking in progress
+}
+console.log(integerBreak(10))
